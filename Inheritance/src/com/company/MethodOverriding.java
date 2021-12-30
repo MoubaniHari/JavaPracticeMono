1915ffba4dbb9a7954ee10897d9e864a470871f8
Moubani Hari <h.moubani23@gmail.com>
2021-12-30 19:36:27 +0530

    It contains: MethodOverriding

--- Inheritance/src/com/company/MethodOverriding.java
+++ Inheritance/src/com/company/MethodOverriding.java
@@ -1,19 +1,29 @@
 package com.company;
-class Super{
-    public void display(){
-        System.out.println("Super class.");
+class Car{
+    public void start(){
+        System.out.println("Car is started.");
+    }
+    public void accelerate(){
+        System.out.println("Car is accelerated.");
+    }
+    public void gearChange(){
+        System.out.println("Car gear changed.");
     }
 }
-class Sub extends Super{
-    public void display(){
-        System.out.println("Sub class.");
+class LuxuryCar extends Car{
+    public void gearChange(){
+        System.out.println("Automatically gear changed.");
+    }
+    public void openRoof(){
+        System.out.println("Roof is opened.");
     }
 }
 public class MethodOverriding {
     public static void main(String[] args) {
-        Super sup = new Super();
-        sup.display();
-        Sub s = new Sub();
-        s.display();
+        LuxuryCar l = new LuxuryCar();
+        l.start();
+        l.accelerate();
+        l.gearChange();
+        l.openRoof();
     }
 }
